8cd78677b36f7507df94831d9b71101d3b781d24
Vivek Sondhiya <viveksondhiya555@gmail.com>
2024-07-09 00:13:45 +0530

    Main method working fine with sample test case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Template for Magical Arena Assignment !");
+       // System.out.println("Template for Magical Arena Assignment !");
+        Player player1 = new Player(50, 5, 10);
+        Player player2 = new Player(100, 10, 5);
+
+        Arena arena = new Arena(player1, player2);
+        arena.fight();
     }
 }
